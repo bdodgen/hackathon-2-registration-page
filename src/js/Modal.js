cf8c5a44e00e3960d801f94624f68505e00b034f
Aidan <aidan.aitchison@gmail.com>
2022-09-30 15:13:59 +0200

    Added fetch POST to modal, removed HTML form elements.

--- src/js/Modal.js
+++ src/js/Modal.js
@@ -6,20 +6,67 @@ export class Modal {
         this.image_url = image_url
 
         const modalSelect = document.querySelector('.modal')
-        modalSelect.style.display = "block"
-    }
+        modalSelect.innerHTML = `
+            <div class="modal__content" >
+            <span class="modal__close">&times;</span>
+            <form>
+                <label for="fname">First name:</label>
+                <input type="text" id="fname" name="fname">
 
-    eventRegister() {
-        const closeSelect = document.querySelector('.modal__close')[0]
+                <label for="lname">Last name:</label>
+                <input type="text" id="lname" name="lname">
+
+                <label for="email">Email adress:</label>
+                <input type="text" id="email" name="email">
+
+                <label for="pnum">Phone number:</label>
+                <input type="text" id="pnum" name="pnum">
+
+                <label for="age">I'm old enough to participate</label>
+                <input type="checkbox" id="age" name="age">
+
+                <button class="modal__register">Register</button>
+            </form>
+        </div>
+        `
+        modalSelect.style.display = "block"
+        const closeSelect = document.querySelector('.modal__close')
         closeSelect.addEventListener('click', () => {
             modalSelect.style.display = "none"
         })
-        window.addEventListener('click', () => {
-            modalSelect.style.display = "none"
+        modalSelect.addEventListener('click', (event) => {
+            if (event.target == modalSelect) {
+                modalSelect.style.display = "none"
+            }
         })
     }
 
 
+    sendForm = async () => {
+        let fname = document.getElementByID('fname').value
+        let lname = document.getElementByID('lname').value
+        let email = document.getElementByID('email').value
+        let pnum = document.getElementByID('pnum').value
+        let age = document.getElementByID('age').value
+
+        const url = 'https://test-api.codingbootcamp.cz/api/cfb0622e/events/EVENT_ID/registrations'
+        const formDataToPass = {
+            "fname": `${fname}`,
+            "lname": `${lname}`,
+            "email": `${email}`,
+            "pnum": `${pnum}`,
+            "age": `${age}`
+        }
+        const myResponse = await fetch(url, {
+            "method": "POST",
+            "body": JSON.stringify(formDataToPass),
+            "headers": {
+                'Content-Type': 'application/json'
+            }
+        })
+        const myUsableResponse = await myResponse.json()
+        console.log(myUsableResponse)
+    }
 }
 
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,7 @@
 import { Modal } from './Modal.js'
 
-const buttonSelect = document.querySelector('button')
+const buttonSelect = document.querySelector('.button')
 buttonSelect.addEventListener('click', () => {
     const newModal = new Modal()
 })
 
-
-
-const LoadData = async () => {
-    const response = await fetch('https://test-api.codingbootcamp.cz/api/36a9d4de/events')
-    const data = await response.json();
-
-    console.log(data)
-}
-
-LoadData()
